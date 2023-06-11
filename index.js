e18c96340e112d4d2190e800f6722d9725112c55
Masrafi404 <mdmasrafi902@gmail.com>
2023-06-11 08:50:35 +0600

    instructor role

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.BD_USER}:${process.env.DB_PASS}@cluster0.icictvn.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.icictvn.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -77,20 +77,22 @@ async function run() {
             res.send(result)
         })
 
+
         // user post
         app.post('/users', async (req, res) => {
             const user = req.body;
-            console.log(user)
-            const query = { email: user.email }
+            console.log(user);
+            const query = { email: user.email };
             const existingUser = await usersCollection.findOne(query);
-            console.log('existing', existingUser)
+            console.log('existing', existingUser);
             if (existingUser) {
-                return res.send({ message: 'user already exist' })
+                return res.send({ message: 'user already exists' }); // Added return statement here
             }
 
             const result = await usersCollection.insertOne(user);
-            res.send(result)
-        })
+            res.send(result);
+        });
+
 
         // users get
         app.get('/users', async (req, res) => {
@@ -120,6 +122,21 @@ async function run() {
             const result = await usersCollection.updateOne(filter, updateDoc);
             res.send(result)
         })
+
+
+        // set instructor
+        app.patch('/users/instructor/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    role: 'instructor'
+                },
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.send(result)
+        })
+
 
 
         // user delete
